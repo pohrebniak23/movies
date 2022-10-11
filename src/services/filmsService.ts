faeece462bbb5220d61da339d72018fa4400bda4
Oleksandr <pogrebnyak19999@gmail.com>
2022-10-11 12:40:02 +0200

    Fix films token

--- src/services/filmsService.ts
+++ src/services/filmsService.ts
@@ -11,33 +11,33 @@ export const filmsApi = createApi({
     getLatestContent: builder.query<IMoviesData, IQueryLatestContent>({
       query: ({ category, limit }) => ({
         url: `/movie?field=rating.kp&search=5-10&field=type&search=${category}&field=year&search=${new Date().getFullYear()}&limit=${limit}&token=${
-          process.env.REACT_APP_TOKEN
+          process.env.REACT_APP_FILMS_TOKEN
         }`,
       }),
     }),
     getContentByCategory: builder.query<IMoviesData, IQueryContentByCategory>({
       query: ({ category, year, rating, genre, limit }) => ({
-        url: `/movie?${genre}&field=rating.kp&search=${rating.min}-${rating.max}&field=type&search=${category}&field=year&search=${year.min}-${year.max}&limit=${limit}&token=${process.env.REACT_APP_TOKEN}`,
+        url: `/movie?${genre}&field=rating.kp&search=${rating.min}-${rating.max}&field=type&search=${category}&field=year&search=${year.min}-${year.max}&limit=${limit}&token=${process.env.REACT_APP_FILMS_TOKEN}`,
       }),
     }),
     getFilmByName: builder.query<IMoviesData, string>({
       query: (name) => ({
-        url: `/movie?field=name&search=${name}&isStrict=false&token=${process.env.REACT_APP_TOKEN}`,
+        url: `/movie?field=name&search=${name}&isStrict=false&token=${process.env.REACT_APP_FILMS_TOKEN}`,
       }),
     }),
     getFilmById: builder.query<IMovieInfo, string | undefined>({
       query: (id) => ({
-        url: `/movie?field=id&search=${id}&token=${process.env.REACT_APP_TOKEN}`,
+        url: `/movie?field=id&search=${id}&token=${process.env.REACT_APP_FILMS_TOKEN}`,
       }),
     }),
     getActorById: builder.query<IActor, string | undefined>({
       query: (id) => ({
-        url: `/person?field=id&search=${id}&token=${process.env.REACT_APP_TOKEN}`,
+        url: `/person?field=id&search=${id}&token=${process.env.REACT_APP_FILMS_TOKEN}`,
       }),
     }),
     getActorImageById: builder.query<any, number>({
       query: (id) => ({
-        url: `/image?field=movieId&search=${id}&field=type&search=backdrops&token=${process.env.REACT_APP_TOKEN}`,
+        url: `/image?field=movieId&search=${id}&field=type&search=backdrops&token=${process.env.REACT_APP_FILMS_TOKEN}`,
       }),
     }),
   }),
